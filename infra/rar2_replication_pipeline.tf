90524913c648b00af7453ff9592574248e73129b
Kane Smith <kane.smith@gov.bc.ca>
2024-02-02 11:48:14 -0800

    remove delivery directory. outside poc scope

--- infra/rar2_replication_pipeline.tf
+++ infra/rar2_replication_pipeline.tf
@@ -17,8 +17,3 @@ resource "aws_s3_object" "rar2_raw" {
   bucket = module.data_hub_bucket.s3_bucket_id
   key = "${aws_s3_object.rar2.key}raw/"
 }
-
-resource "aws_s3_object" "rar2_delivery" {
-  bucket = module.data_hub_bucket.s3_bucket_id
-  key = "${aws_s3_object.rar2.key}delivery/"
-}
