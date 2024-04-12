9f61df245d84d11957fc0f5da1f9902d55e85f7f
Kane Smith <kane.smith@gov.bc.ca>
2024-04-12 16:29:11 -0700

    remove infra

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -1,18 +1,18 @@
-terraform {
-  backend "s3" {
-    region = "ca-central-1"
-  }
+# terraform {
+#   backend "s3" {
+#     region = "ca-central-1"
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.44.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.44.0"
+#     }
+#   }
 
-  required_version = ">= 1.6.2"
-}
+#   required_version = ">= 1.6.2"
+# }
 
-provider "aws" {
-  region = "ca-central-1"
-}
+# provider "aws" {
+#   region = "ca-central-1"
+# }
